a6f77d125e0d60725ae50d5325f95055db350f39
aad7700 <aad7700@g.rit.edu>
2023-06-15 16:49:59 -0400

    Fixed gravity and Jumping

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,14 +7,20 @@ public class PlayerMovement : MonoBehaviour
     [Header("Movement")]
     public float moveSpeed;
 
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMult;
+    bool readyToJump;
+
     [Header("Ground Check")]
     public float playerHeight;
     public LayerMask whatIsGround;
     public bool grounded;
     public float groundDrag;
+    public Transform groundCheckTrans;
 
     public Transform orientation;
-    
+
     float horizontalInput;
     float verticalInput;
 
@@ -22,25 +28,44 @@ public class PlayerMovement : MonoBehaviour
 
     public Rigidbody rig;
 
+    public Vector3 gravForce;
     // Start is called before the first frame update
     void Start() {
+        
         rig.GetComponent<Rigidbody>();
         rig.freezeRotation = true;
+        readyToJump = true;
+        
     }
 
 
     // Update is called once per frame
     private void Update() {
-
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+        Physics.gravity = gravForce;
+        grounded = Physics.Raycast(groundCheckTrans.position, Vector3.down, 0.1f, whatIsGround);
 
         movePlayer();
         speedControl();
 
-        if (grounded)
+        if (Input.GetKey(KeyCode.Space) && readyToJump && grounded) {
+
+            readyToJump = false;
+            Jump();
+            Invoke(nameof(resetJump), jumpCooldown);
+        
+        }
+
+
+        if (grounded && rig.velocity == new Vector3(0f, 0f, 0f))
+        {
             rig.drag = groundDrag;
-        else
-            rig.drag = 0;
+            readyToJump = true;
+        }
+        else {
+
+            rig.drag = 0f;
+        
+        }
     }
 
 
@@ -53,8 +78,15 @@ public class PlayerMovement : MonoBehaviour
         //caculate movement
         moveDir = (orientation.forward * verticalInput + orientation.right * horizontalInput) * Time.deltaTime;
 
-        rig.AddForce(moveDir.normalized * moveSpeed * 10f, ForceMode.Force);
-
+        if (grounded)
+        {
+            rig.AddForce(moveDir.normalized * moveSpeed * 10f, ForceMode.Force);
+        }
+        else
+        {
+            rig.AddForce(moveDir.normalized * moveSpeed * 10f * airMult, ForceMode.Force);
+        }
+         
     }
 
     private void speedControl() {
@@ -68,5 +100,17 @@ public class PlayerMovement : MonoBehaviour
 
         }
 
+    }
+
+    private void Jump() {
+
+        rig.velocity = new Vector3(rig.velocity.x, 0f, rig.velocity.z);
+
+        rig.AddForce(transform.up * jumpForce, ForceMode.Impulse);    
+    
+    }
+
+    private void resetJump() { 
+        readyToJump = true;
     }
 }
