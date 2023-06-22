07dce1d0067413e3cd635cea568b2becf66a0ca3
aad7700 <aad7700@g.rit.edu>
2023-06-22 10:32:33 -0400

    started working on the hover, it either oscilates or doesn't hover

--- Assets/SlicerMovement.cs
+++ Assets/SlicerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,20 +6,45 @@ using UnityEngine;
 public class SlicerMovement : MonoBehaviour
 {
     public Rigidbody rig;
-    
+    public Transform trans;
+
+    [Header("Hover")]
+    public float hoverHeight = 1.5f;
+    public float hoverForce = 10f;
+    public float gravForce = 9.81f;
+    public float hoverForceLimit = 100f;
+
     // Start is called before the first frame update
     void Start()
     {
-        rig.GetComponent<Rigidbody>();
+        rig = GetComponent<Rigidbody>();
+        trans = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.y < 1.5f) {
-            transform.position = new Vector3(transform.position.x, 1.5f, transform.position.z);
-        
-        
+        Hover();
+    }
+
+    private void Hover()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(trans.position, -trans.up);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            float proportionalHeight = hoverHeight - hit.distance;
+
+            // Limit hover force when close to the hover height
+            float limitedHoverForce = Mathf.Clamp(proportionalHeight * hoverForce, -hoverForceLimit, hoverForceLimit);
+
+            Vector3 appliedHoverForce = Vector3.up * limitedHoverForce;
+            rig.AddForce(appliedHoverForce, ForceMode.Force);
+        }
+        else
+        {
+            rig.AddForce(Vector3.down * gravForce, ForceMode.Force);
         }
     }
 }
